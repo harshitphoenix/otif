ea07b8261941f28a0cf455e34a6fb952b96842f8
harshitpheonix <img_2019022@iiitm.ac.in>
2022-06-16 04:00:53 +0530

    Nav bar updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={`${styles.container} navbar navbar-expand-lg`}>
       <div className='container-fluid'>
-       
+        <a className={`${styles.navItem} nav-link `} href='#'>
+          Help
+        </a>
+        <a className={`${styles.navItem} nav-link `} href='#'>
+          SignIn
+        </a>
+
         <button
           className='navbar-toggler'
           type='button'
@@ -18,64 +24,36 @@ const Navbar = () => {
         </button>
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='#'>
-                Home
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>
-                Link
-              </a>
-            </li>
-
-            <li className='nav-item'>
-              <a
-                className='nav-link disabled'
-                href='#'
-                // tabindex='-1'
-                aria-disabled='true'
-              >
-                Disabled
-              </a>
-            </li>
-          </ul>
-          <form className='d-flex'>
             <li className='nav-item dropdown'>
               <a
-                className='nav-link dropdown-toggle'
+                className={`${styles.navItem} nav-link dropdown-toggle`}
                 href='#'
                 id='navbarDropdown'
                 role='button'
                 data-bs-toggle='dropdown'
                 aria-expanded='false'
               >
-                Dropdown
+                English
               </a>
               <ul className='dropdown-menu' aria-labelledby='navbarDropdown'>
                 <li>
                   <a className='dropdown-item' href='#'>
-                    Action
+                    Spanish
                   </a>
                 </li>
                 <li>
                   <a className='dropdown-item' href='#'>
-                    Another action
+                    Portuguese
                   </a>
                 </li>
                 <li>
-                  <hr className='dropdown-divider' />
-                </li>
-                <li>
                   <a className='dropdown-item' href='#'>
-                    Something else here
+                    Japanese
                   </a>
                 </li>
               </ul>
             </li>
-            {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
-            {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
-          </form>
+          </ul>
         </div>
       </div>
     </nav>
